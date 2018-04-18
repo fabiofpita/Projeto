309c18c4e3c1ffd887c75d843e12bc94e1319de6
George Souza <george_souza97@outlook.com>
2018-04-17 22:04:53 -0300

    Metodo adcionados a classe Computador

--- SistemaDomino/src/computador/Computador.java
+++ SistemaDomino/src/computador/Computador.java
@@ -5,10 +5,40 @@ package computador;
  */
 public class Computador {
 
+    private long score;
+    private int vitorias;
+    private int derrotas;
+    private String nome;
+
     /**
      * Construtor de um objeto do tipo computador.
      */
     public Computador(){
+        vitorias = 0;
+        derrotas = 0;
+        score = 0;
+        nome = "Caio";
     }
 
+    /**
+     * @return Metodo retorna o nome do computador.
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @return Retorna o score do jogador.
+     */
+    public long getScore() {
+        return this.score;
+    }
+
+    /**
+     * Acrescenta ao score do jogador.
+     * @param info Valor a ser somado ao antigo score.
+     */
+    public void setScore(long info) {
+        this.score += info;
+    }
 }
